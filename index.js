88c7268b69b7381dfb66a6946e28d47eca157c38
sktanui <shadrackkemei2019@gmail.com>
2024-10-03 14:42:19 +0300

    complete DOM modiftying element lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.id = 'victory';
+newHeader.textContent = "Shadrack is the champion";
+document.body.appendChild(newHeader);
